e173da24b13bfcb4e698176c1e33b618fdb8871a
taiwoj121 <taiwoj121@gmail.com>
2024-05-23 22:39:33 +0100

    fix links to different section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flexCenter mb-24">
+    <footer id='contact' className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <div className="font-bold text-xl text-transform uppercase">
+                Trave<span className="text-green-500">xy</span>
+               </div>
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,10 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_hilink_work', label: 'How TravexyWork?' },
-    { href: '/', key: 'services', label: 'Services' },
+    { href: '#home', key: 'home', label: 'Home' },
+    { href: '#how', key: 'how_Travexy_work', label: 'How TravexyWork?' },
+    { href: '#services', key: 'services', label: 'Services' },
     { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
+    { href: '#contact', key: 'contact_us', label: 'Contact Us' },
   ];
   
   // CAMP SECTION
